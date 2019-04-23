651b5bdf505fb8d08cd70164f51960a8251b8024
Noah <17747271+gTX31vsg@users.noreply.github.com>
2019-04-23 23:39:40 +0200

    Remove output

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _App = require('./App');
-
-var _App2 = _interopRequireDefault(_App);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-new _App2.default();
